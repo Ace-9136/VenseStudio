87f1641f1217f6925e7da0ab52eeec5fc38ba138
Brett <sahilbagate8108@gmail.com>
2025-02-08 11:24:32 +0530

    added insta page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Hero() {
   return (
     <section className="hero">
       <div className="container">
         <h1>Welcome to VenseStudio</h1>
-        <p>A collective of passionate student artists</p>
-        <a href="#students" className="cta-button">Meet the Artists</a>
+        <p>A collective of passionate student artists.<br /> Join us in our creative journey!</p>
+        <a href="https://instagram.com/_vense_studio_" className="cta-button lm"><InstagramIcon style={{ width: "20px", height: "20px", marginRight: "8px", marginBottom: "2px" }} />Follow Us</a>
+        <a href="#students" className="cta-button">
+Meet the Artists</a>
       </div>
     </section>
   );
